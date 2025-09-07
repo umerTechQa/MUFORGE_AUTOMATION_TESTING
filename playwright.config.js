cc81f13c9434033b4d194565378a10d307934637
Umer Tech QA <ommyansari@gmail.com>
2025-09-08 02:15:36 +0500

    new changes for capturing recording videos on every runs

--- playwright.config.js
+++ playwright.config.js
@@ -30,6 +30,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: true,
+  video: 'on',
+  screenshot: 'only-on-failure',
     trace: 'on-first-retry',
   },
 
